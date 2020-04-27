bdc9cfb1b4d435f1acdc91a6f5d95790c6213836
ivanzhang1008 <815327985@qq.com>
2020-04-27 11:00:11 +0800

    uniapp-vue test

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,27 @@
 <template>
 	<view class="page">
-		
+		<!-- 轮播图 start -->
 		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
 			<swiper-item v-for="carousel in carouselList" :key=carousel.id >
 				<image :src="carousel.image" class="carousel"></image>
 			</swiper-item>
 		</swiper>
+	<!-- 轮播图 end -->
 	
+	<!-- 热门超英 start-->
+	<view class="page-block super-hot" >
+		<view class="hot-title-wapper">
+			<image src="../../herotest/static/icos/hot.png" class="hot-ico" mode=""></image>
+			<view class="hot-title">
+				热门超英
+			</view>
+			
+			
+		</view>
+		
+	</view>
+	
+	<!-- 热门超英 end-->
 	</view>
 </template>
 
